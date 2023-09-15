479b8e4eb8009315c304c639f4e5c6cd7e6a8e05
TheXiShuai <johndoe@example.com>
2023-09-15 19:38:03 +0200

    stack-4 project day1

---
+++ src/Components/GameCircle.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../Game.css";
+
+const onClick = (ev, id) => {
+  alert("on click" + id);
+};
+
+const GameCircle = ({ id, color, children }) => {
+  const style = {
+    backgroundColor: color,
+   
+  };
+  return (
+    <div className="gameCircle" style={style} onClick={(ev) => onClick(ev, id)}>
+      {children}
+    </div>
+  );
+};
+
+export default GameCircle;
